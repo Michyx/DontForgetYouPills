2611380ef5699e43a36d91e0153faf62a34e9455
Jessx3x3 <j.vasquez12@ufromail.cl>
2017-06-14 14:47:37 -0400

    JLabel Hora

--- src/main/java/views/PanelProxAlarma.java
+++ src/main/java/views/PanelProxAlarma.java
@@ -1,21 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package views;
 
 import java.awt.FlowLayout;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
-import javax.swing.JTextField;
 
-/**
- *
- * @author Loli Pop
- */
+
+
 public class PanelProxAlarma extends JPanel {
-    private JTextField ingresar;
+    private JLabel ingresar;
+    private Date fecha = new Date();
+    private String hora;
 
     public PanelProxAlarma() {
         initComponents();
@@ -26,7 +25,11 @@ public class PanelProxAlarma extends JPanel {
         FlowLayout distribucion = new FlowLayout();
         this.setLayout(distribucion);   
         JSeparator separator = new JSeparator();
-        this.ingresar = new JTextField("Proxima Alarma : 13:00");
+        
+        DateFormat formatoHora = new SimpleDateFormat("HH:mm:ss");
+        hora = (formatoHora.format(fecha));
+        
+        this.ingresar = new JLabel(hora);
         this.add(separator);
         this.add(this.ingresar);
         
